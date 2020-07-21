f078b4ab247474fc07d6a75f7bea3b7a9485a071
Aidan Holland <thehappydinoa@gmail.com>
2020-07-21 10:31:54 -0400

    test: mock api server

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,24 +1,60 @@
 import datetime
 
+import pytest
+from pytest_httpserver import HTTPServer
+
 from darksearch import Client
 
-client = Client(timeout=10)
+PORT = 4000
 
 
-def test_search_page_1():  # noqa: D103
-    response = client.search("query")
+def _setup_all(server: HTTPServer):
+    server.expect_request(
+        "/api/search", query_string="query=query&page=1"
+    ).respond_with_json({"current_page": 1, "last_page": 5})
+
+    server.expect_request(
+        "/api/search", query_string="query=query&page=2"
+    ).respond_with_json({"current_page": 2, "last_page": 5})
+
+    server.expect_request(
+        "/api/search",
+        query_string="query=super_specific_search_page_no_one_will_create_1234&page=1",
+    ).respond_with_json({"current_page": 1, "last_page": 0})
+
+    server.expect_request("/api/crawling_status").respond_with_data(str(1090214))
+
+
+@pytest.fixture
+def httpserver_listen_address():
+    return ("127.0.0.1", PORT)
+
+
+@pytest.fixture
+def darksearch_client():
+    base_url = f"http://localhost:{PORT}"
+    return Client(base_url=base_url, timeout=10)
+
+
+def test_search_page_1(httpserver: HTTPServer, darksearch_client: Client):  # noqa: D103
+    _setup_all(httpserver)
+    response = darksearch_client.search("query")
     assert isinstance(response, dict)
     assert response.get("current_page") == 1
 
 
-def test_search_page_2():  # noqa: D103
-    response = client.search("query", page=2)
+def test_search_page_2(httpserver: HTTPServer, darksearch_client: Client):  # noqa: D103
+    _setup_all(httpserver)
+    response = darksearch_client.search("query", page=2)
     assert isinstance(response, dict)
     assert response.get("current_page") == 2
 
 
-def test_search_2_pages():  # noqa: D103
-    response = client.search("query", pages=2)
+def test_search_2_pages(
+    httpserver: HTTPServer, darksearch_client: Client
+):  # noqa: D103
+    _setup_all(httpserver)
+    response = darksearch_client.search("query", pages=2)
     assert isinstance(response, list)
     assert len(response) == 2
     response_1, response_2 = response
@@ -28,9 +64,12 @@ def test_search_2_pages():  # noqa: D103
     assert response_2.get("current_page") == 2
 
 
-def test_search_2_pages_wait():  # noqa: D103
+def test_search_2_pages_wait(
+    httpserver: HTTPServer, darksearch_client: Client
+):  # noqa: D103
+    _setup_all(httpserver)
     start = datetime.datetime.now()
-    response = client.search("query", pages=2, wait=10)
+    response = darksearch_client.search("query", pages=2, wait=10)
     end = datetime.datetime.now()
     elapsed = end - start
     assert isinstance(response, list)
@@ -43,14 +82,20 @@ def test_search_2_pages_wait():  # noqa: D103
     assert elapsed.seconds >= 10
 
 
-def test_search_last_page():  # noqa: D103
-    response = client.search(
-        "super_specific_search_page_no_one_will_create", pages=5
+def test_search_last_page(
+    httpserver: HTTPServer, darksearch_client: Client
+):  # noqa: D103
+    _setup_all(httpserver)
+    response = darksearch_client.search(
+        "super_specific_search_page_no_one_will_create_1234", pages=5
     )
     assert isinstance(response, list)
     assert len(response) == 1
 
 
-def test_crawling_status():  # noqa: D103
-    response = client.crawling_status()
+def test_crawling_status(
+    httpserver: HTTPServer, darksearch_client: Client
+):  # noqa: D103
+    _setup_all(httpserver)
+    response = darksearch_client.crawling_status()
     assert isinstance(response, int)
