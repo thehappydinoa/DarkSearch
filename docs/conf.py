2c1adda0e823e037a0dc7a561f96aa95e581a608
Aidan Holland <thehappydinoa@gmail.com>
2021-04-29 22:09:36 -0400

    chore: bump version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Aidan Holland"
 author = "Aidan Holland"
 
 # The full version, including alpha/beta/rc tags
-release = "2.1.0"
+release = "2.1.1"
 
 
 # -- General configuration ---------------------------------------------------
